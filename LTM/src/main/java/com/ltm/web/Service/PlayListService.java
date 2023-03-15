bd99d0b8ac2c7c67ca7fa960c11e51d7d1e13173
scars97 <qkr970504@gmail.com>
2023-03-16 00:17:18 +0900

    플레이리스트 화면수정 ~ing

--- LTM/src/main/java/com/ltm/web/Service/PlayListService.java
+++ LTM/src/main/java/com/ltm/web/Service/PlayListService.java
@@ -79,7 +79,7 @@ public class PlayListService {
 
 	// 전체 플레이리스트
 	public Page<PlayList> getlist(int page) {
-		Pageable pageable = PageRequest.of(page, 2); // page는 조회할 페이지의 번호, 2는 한페이지에 보여줄 게시물의 개수
+		Pageable pageable = PageRequest.of(page, 9); // page는 조회할 페이지의 번호, 2는 한페이지에 보여줄 게시물의 개수
 		return playListRepository.findAll(pageable);
 		// 목록을 조회하여 리턴하는 getlist 메서드 추가. 컨트롤러에서 레포지토리를 사용했던 부분을 그대로 옮긴 것
 		// 엔티티클래스와 데이터베이스가 직접맞닿아서 컨트롤러나 타임리프같은 템플릿 엔진에 전달하여 사용하는 것은 좋지 않기 때문에 서비스를 사용
@@ -87,7 +87,7 @@ public class PlayListService {
 
 	// 검색 결과 플레이리스트
 	public Page<PlayList> getlistkeyword(int page, String kw) {
-		Pageable pageable = PageRequest.of(page, 2); // page는 조회할 페이지의 번호, 2는 한페이지에 보여줄 게시물의 개수
+		Pageable pageable = PageRequest.of(page, 9); // page는 조회할 페이지의 번호, 2는 한페이지에 보여줄 게시물의 개수
 		return playListRepository.findByKeyword(kw, pageable);
 	}
 
