8b535ed9525bf86c3e202f9c20d89c1881581cdc
1eeseunghun <g1eeseunghun@gmail.com>
2023-03-20 08:54:18 +0900

    게시판 또 수정

--- LTM/src/test/java/com/ltm/web/LtmApplicationTests.java
+++ LTM/src/test/java/com/ltm/web/LtmApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.ltm.web.Service.AdboardService;
 import com.ltm.web.Service.CboardService;
 
 @SpringBootTest
@@ -11,9 +12,10 @@ class LtmApplicationTests {
 	
 	@Autowired
 	private CboardService cboardService;
+	/*private AdboardService adboardService;*/
 	
 	 @Test
-     void 게시글_생성() {
+     void 커뮤_니티() {
          for (int i = 1; i <= 100; i++) {
              String ctitle = String.format("테스트 데이터입니다:[%03d]", i);
              String cbody = "내용무";
@@ -21,4 +23,14 @@ class LtmApplicationTests {
              this.cboardService.create(ctitle, cbody, null, tags);
          }
      }
+	
+/*	@Test
+	void 공지_사항() {
+		for (int i=1; i<= 40; i++) {
+			String adtitle = String.format("[공지사항]공지사항입니다:[%03d]", i);
+			String adbody = "내용무";
+			String tags = "해시태그";
+			this.adboardService.create(adtitle, adbody, null, tags);
+		}
+	}*/
 }
